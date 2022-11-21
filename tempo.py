90f67b343b0c0c7768be14d0109e072da9dad603
rocco1967 <gianfranco.fa@gmail.com>
2022-11-21 21:40:58 +0100

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -34,7 +34,7 @@ new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;"</p
 st.markdown(new_title, unsafe_allow_html=True)
 st.dataframe(b,300,200)
 image = Image.open('b')
-st.image(image
+st.image('image')
 #st.markdown('-----')
 #st.dataframe(b['Milano'],300,200)
 #print(b)
