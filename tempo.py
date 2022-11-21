63d41646fc66babe1a5550aaca26e241e569a4f6
rocco1967 <gianfranco.fa@gmail.com>
2022-11-21 21:26:58 +0100

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -21,10 +21,11 @@ url_list=('https://wttr.in/Gdynia?m&format=%t','https://wttr.in/Gdynia?format=%h
          'https://wttr.in/Milan?m&format=%t','https://wttr.in/Milan?format=%h','https://wttr.in/Milan?m&format=%f','https://wttr.in/Milan?format=%s')
 for url_list in url_list:
     dati=((requests.get(url_list).text))
+    requests.close()
     np.array(dati1.append(dati))
     #np.append(print(requests.get(dati).text))
 #b=pd.DataFrame(np.array(dati1),columns=['TEMP','TEMP-PERCEPITA'])#,'UMIDITA^','ORA-LOCALE-DATI'])
-requests.close()
+#requests.close()
 #a=((pd.DataFrame((np.array(dati1).reshape(2,-1)),columns=['TEMP','UMIDITA^'])).T)#,columns=['Gdynia','Milano'])
 a=pd.DataFrame((np.array(dati1).reshape(-1,4)),columns=['temperatura','umidita^','temp-percepita','Sunset'],index=['Gdynia','Milano'])
 b=(a.T)
