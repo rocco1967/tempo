042302395f53c995c7a2ed3a07c765e42d5d27e8
rocco1967 <gianfranco.fa@gmail.com>
2022-11-22 11:26:29 +0100

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -22,7 +22,10 @@ st.markdown(
 )
 dati1=[]
 url_list=('https://wttr.in/Gdynia?m&format=%t&period=60','https://wttr.in/Gdynia?format=%h','https://wttr.in/Gdynia?m&format=%f','https://wttr.in/Gdynia?format=%s',
-         'https://wttr.in/Milan?m&format=%t&period=60','https://wttr.in/Milan?format=%h','https://wttr.in/Milan?m&format=%f','https://wttr.in/Milan?format=%s')
+          'https://wttr.in/Gdynia?format=%S',
+          
+         'https://wttr.in/Milan?m&format=%t&period=60','https://wttr.in/Milan?format=%h','https://wttr.in/Milan?m&format=%f','https://wttr.in/Milan?format=%s',
+         'https://wttr.in/Milan?format=%S')
 for url_list in url_list:
     dati=((requests.get(url_list,headers={'Connection':'close'}).text))
     #requests.close()
@@ -31,7 +34,7 @@ for url_list in url_list:
 #b=pd.DataFrame(np.array(dati1),columns=['TEMP','TEMP-PERCEPITA'])#,'UMIDITA^','ORA-LOCALE-DATI'])
 #requests.close()
 #a=((pd.DataFrame((np.array(dati1).reshape(2,-1)),columns=['TEMP','UMIDITA^'])).T)#,columns=['Gdynia','Milano'])
-a=pd.DataFrame((np.array(dati1).reshape(-1,4)),columns=['temperatura','umidita^','temp-percepita','Sunset'],index=['Gdynia','Milano'])
+a=pd.DataFrame((np.array(dati1).reshape(-1,5)),columns=['temperatura','umidita^','temp-percepita','Sunset','Sunrise'],index=['Gdynia','Milano'])
 b=(a.T)
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;"</p>'
 #st.markdown(new_title, unsafe_allow_html=True)
