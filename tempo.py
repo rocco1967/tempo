57e1fd6e0ce2d6718c056b26b0cfb5b19aab1f02
rocco1967 <gianfranco.fa@gmail.com>
2022-11-22 15:05:46 +0100

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -40,6 +40,7 @@ new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;"</p
 #st.markdown(new_title, unsafe_allow_html=True)
 #st.dataframe(b,300,200)########  originale
 b=(b.reset_index()).rename(columns={'index': 'dati'})##  nuovo
+plt.figure(figsize=(10,5))
 st.pyplot((dataframe_to_image.convert(b,visualisation_library='matplotlib')))
 
 #image = Image.open('b')
