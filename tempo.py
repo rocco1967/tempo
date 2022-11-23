62b9113a8300a248f9f4da9aca56160d99982bad
rocco1967 <gianfranco.fa@gmail.com>
2022-11-23 15:02:26 +0100

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -31,7 +31,7 @@ url_list=('https://wttr.in/Gdynia?m&format=%t&period=5','https://wttr.in/Gdynia?
          'https://wttr.in/Milan?m&format=%t&period=5','https://wttr.in/Milan?format=%h&period=5','https://wttr.in/Milan?m&format=%f','https://wttr.in/Milan?format=%s',
          'https://wttr.in/Milan?format=%S')
 for url_list in url_list:
-    dati=((requests.get(url_list,timeout=2,headers={'Connection':'close'}).text))
+    dati=((requests.get(url_list,timeout=3,headers={'Connection':'close'}).text))
     #requests.close()
     np.array(dati1.append(dati))
     #np.append(print(requests.get(dati).text))
